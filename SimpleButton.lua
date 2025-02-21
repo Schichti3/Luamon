cdb792081179d9d9bb11169bd18f048fbf7ad4b9
Schichti3 <orasdemo123@gmail.com>
2025-02-21 22:10:07 +0100

    reformatted simplebutton.lua

--- SimpleButton.lua
+++ SimpleButton.lua
@@ -1,10 +1,10 @@
-local SimpleButton = setmetatable({}, { __index = require("Element") })
+local SimpleButton = setmetatable({}, { __index = require('Element') })
 
 SimpleButton.width = 100
 SimpleButton.height = 60
 SimpleButton.color = { 0, 0, 0 }
 
-SimpleButton.text = "Text"
+SimpleButton.text = 'Text'
 SimpleButton.textColor = { 1, 1, 1 }
 
 SimpleButton.border = true
@@ -12,44 +12,42 @@ SimpleButton.borderWidth = 2
 SimpleButton.borderColor = { 1, 1, 1 }
 
 function SimpleButton:new(templateTable)
-	local obj = templateTable or {}
-	setmetatable(obj, self)
-	self.__index = self
-	return obj
+  local obj = templateTable or {}
+  setmetatable(obj, self)
+  self.__index = self
+  return obj
 end
 
 function SimpleButton:draw()
-	if self.visible then
-		love.graphics.setColor(self.color[1], self.color[2], self.color[3])
-		love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-		love.graphics.setColor(self.borderColor[1], self.borderColor[2], self.borderColor[3])
-		love.graphics.setLineWidth(self.borderWidth)
-		love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
+  if self.visible then
+    love.graphics.setColor(self.color[1], self.color[2], self.color[3])
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+    love.graphics.setColor(self.borderColor[1], self.borderColor[2], self.borderColor[3])
+    love.graphics.setLineWidth(self.borderWidth)
+    love.graphics.rectangle('line', self.x, self.y, self.width, self.height)
 
-		local newX, newY =
-			require("Utility").getCenteredTextCoordinates(self.text, self.x, self.y, self.width, self.height)
-		love.graphics.setColor(self.textColor[1], self.textColor[2], self.textColor[3])
-		love.graphics.print(self.text, newX, newY)
-	end
+    local newX, newY = require('Utility').getCenteredTextCoordinates(self.text, self.x, self.y, self.width, self.height)
+    love.graphics.setColor(self.textColor[1], self.textColor[2], self.textColor[3])
+    love.graphics.print(self.text, newX, newY)
+  end
 end
 
 function SimpleButton:mouseInArea(mouseX, mouseY)
-	return mouseX >= self.x and mouseX <= self.x + self.width and mouseY >= self.y and mouseY <= self.y + self.height
+  return mouseX >= self.x and mouseX <= self.x + self.width and mouseY >= self.y and mouseY <= self.y + self.height
 end
 
 function SimpleButton:onMouseDown()
-	self.text = "mouseDown!"
+  self.text = 'mouseDown!'
 end
 
 function SimpleButton:onMouseUp()
-	self.text = "mouseUp!"
+  self.text = 'mouseUp!'
 end
 
 function SimpleButton:onClick()
-	self.text = "clicked!"
+  self.text = 'clicked!'
 end
 
 function SimpleButton:onHover(onElement) end
 
 return SimpleButton
-
